fae655b3a7d7be83b53cc71632f2de4a33941d2c
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-08-02 15:24:12 +0200

    Add Calculator component
    Add Calculator components files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Numbers from './components/Calculator';
+import './components/Calculator.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Awesome Person!
-        </p>
-      </header>
+      <Numbers />
     </div>
   );
 }
